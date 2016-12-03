e2f217231b300ac92431b1061a8462a189f013be
Tomas Ptacnik <t@tomor.cz>
2016-12-03 11:36:02 +0100

    Add "Line Up"

---
+++ Arcade/LoopTunnel/04_LineUp.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+To prepare his students for an upcoming game, the sports coach decides to try some new training
+drills. To begin with, he lines them up and starts with the following warm-up exercise: when
+the coach says 'L', he instructs the students to turn to the left. Alternatively, when he
+says 'R', they should turn to the right. Finally, when the coach says 'A', the students
+should turn around.
+
+Unfortunately some students (not all of them, but at least one) can't tell left from right,
+meaning they always turn right when they hear 'L' and left when they hear 'R'. The coach
+wants to know how many times the students end up facing the same direction.
+
+Given the list of commands the coach has given, count the number of such commands after
+which the students will be facing the same direction.
+
+Example
+
+For commands = "LLARL", the output should be
+lineUp(commands) = 3.
+
+Let's say that there are 4 students, and the second one can't tell left from right. In this
+case, only after the second, third and fifth commands will the students face the same direction.
+
+https://codefightsuserpics.s3.amazonaws.com/tasks/lineUp/img/example.png?_tm=1474804663999
+
+Input/Output
+
+[time limit] 4000ms (php)
+[input] string commands
+ */
+
+function lineUp($commands) {
+    $array      = str_split($commands);
+    $counter    = 0;
+    $facingSame = true;
+
+    // handle case for empty input
+    // (str_sprit unfortunately creates array with one empty element)
+    if (empty($commands)) {
+        $array = [];
+    }
+
+    foreach ($array as $letter) {
+        switch ($letter) {
+            case 'L':
+            case 'R':
+                $facingSame = !$facingSame;
+
+                break;
+            case 'A':
+                    // no action needed, just consume the command
+                break;
+        }
+
+        if ($facingSame) {
+            $counter++;
+        }
+    }
+
+    return $counter;
+}
